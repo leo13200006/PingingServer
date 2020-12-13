e1802906e646addfa4a7ea4c01bf129db55e197a
Laukik <laukikchavan101@gmail.com>
2020-12-13 20:36:52 +0530

    Now it takes multiple sites :tada:

--- src/main/java/com/personal/piningprogram/PiningprogramApplication.java
+++ src/main/java/com/personal/piningprogram/PiningprogramApplication.java
@@ -8,27 +8,33 @@ import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PiningprogramApplication {
     public static void main(String[] args) throws InterruptedException, IOException {
         SpringApplication.run(PiningprogramApplication.class, args);
+        ArrayList<URL> siteURLs = new ArrayList<>();
+        siteURLs.add(new URL("https://fasim.herokuapp.com/"));
+        siteURLs.add(new URL("https://mesmovies.herokuapp.com/"));
+
         while(true) {
-            URL siteURL = new URL("https://fasim.herokuapp.com/");
-            HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setConnectTimeout(3000);
-            connection.connect();
+            for(URL siteURL : siteURLs) {
+                HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(3000);
+                connection.connect();
 
-            int code = connection.getResponseCode();
-            if (code == 200) {
-                GetClass.incCount();
-                System.out.println("Ping");
-            } else {
-                System.out.println("Can't Ping");
+                int code = connection.getResponseCode();
+                if (code == 200) {
+                    GetClass.incCount();
+                    System.out.println("Ping");
+                } else {
+                    System.out.println("Can't Ping");
+                }
+
+                Thread.sleep(2000);
             }
-
-            Thread.sleep(2000);
         }
     }
 
